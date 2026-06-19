f1d8a4bbdff43fddf55c092e3e8763795296015c
omar shaikh <syed15-8699@diu.edu.bd>
2026-06-19 20:47:21 +0600

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/portfolio-site/',
+  base: "/portfolio-omar/",
   plugins: [
     tailwindcss(),
     react(),
     babel({ presets: [reactCompilerPreset()] }),
   ],
-})
+});
